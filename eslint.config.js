619ffbffdb389570822143a0c976c3527fe1dab5
daniel.ribas <daniel.ribas@isdbflowtech.com>
2026-03-10 16:58:55 -0300

    create: nextcoach-ai app

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,19 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config")
+const expoConfig = require("eslint-config-expo/flat")
+const prettier = require("eslint-config-prettier")
+const prettierPlugin = require("eslint-plugin-prettier")
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
-]);
+   expoConfig,
+   prettier,
+   {
+      plugins: {
+         prettier: prettierPlugin,
+      },
+      rules: {
+         "prettier/prettier": "error",
+      },
+      ignores: ["dist/*"],
+   },
+])
